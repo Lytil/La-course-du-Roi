fa06d5dc040ae5391f2da73164f597ef958fddeb
ycbz <113522559+ycbz@users.noreply.github.com>
2022-10-26 09:51:24 +0200

    Add files via upload

---
+++ Yaël/Admin.php
@@ -0,0 +1,17 @@
+<?php
+    $db=new PDO('mysql:host=localhost;port=3306;dbname=lcdr','root','');
+?>
+
+<!doctype HTML>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Administration</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    siuuuu
+    </body>
+</html>
