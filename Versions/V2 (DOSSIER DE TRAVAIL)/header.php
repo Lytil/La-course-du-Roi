4c2f27505bf09740568a25d0dad49ddd81c40c54
Lytil <113536692+Lytil@users.noreply.github.com>
2022-10-27 11:15:52 +0200

    Creation V2

---
+++ Versions/V2 (DOSSIER DE TRAVAIL)/header.php
@@ -0,0 +1,16 @@
+<html>
+    <header>
+        <div class="placementlogo">
+            <a href="index.php" ><img class="logo" src="image/logo2.png"></a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li><a href="#">Formulaire</a></li>
+                <li><a href="confrerie.html">Confreries</a></li>
+                <li><a href="epreuve.html">Epreuves</a></li>
+                <li><a href="#">Programme</a></li>
+            </ul>
+        </div>
+    </header>
+</html>
