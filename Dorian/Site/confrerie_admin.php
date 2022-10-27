127f445767cc7f6eb5fc3f66730da59a461df6dd
Wiseven <113523138+Wiseven@users.noreply.github.com>
2022-10-27 12:12:47 +0200

    Add files via upload

---
+++ Dorian/Site/confrerie_admin.php
@@ -0,0 +1,71 @@
+<?php
+    $db=new
+    PDO('mysql:host=localhost;port=3306;dbname=lcdr','root','');
+    $results=$db->query('SELECT nomConfrerie, bio FROM confrerie');
+    $tab=$results->fetchAll();
+    $results->closeCursor();
+    $confreries=count($tab);
+
+    class Confrerie
+    {
+        public $nomConfrerie="";
+        public $bio="";
+
+        public function __construct($nomConfrerie, $bio)
+        {
+            $this->nomConfrerie=$nomConfrerie;
+            $this->bio=$bio;
+        }
+
+        public function affichage()
+        {
+            echo '<b class="conf-titre">'.$this->nomConfrerie.'</b><div class="espace"><p><br>'.$this->bio.'<br><br><p></div>';
+        }
+    }
+?>
+
+
+
+<!doctype HTML>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Espace d'administration</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/conf-pres.css">
+    </head>
+    <body>
+        <h1>Espace d'administration</h1>
+        <form method="POST" action="Admin2.php">
+        <div class="bouton-admin">
+            <label>Veuillez sélectionner une table de la base de données à consulter :</label>
+            <select id="select" name="select">
+                <option value="Admin.php">-</option>
+                <option value="CaisseSavon2.php">Caisses à Savon</option>
+                <option value="Categorie2.php">Catégories</option>
+                <option value="Confrerie2.php" selected>Confréries</option>
+                <option value="Parcours2.php">Parcours</option>
+                <option value="Participant2.php">Participants</option>
+            </select>
+            <button id="button">Consulter</button>
+        </div>
+        </form>
+        <br>
+        <h1>Présentation des Confréries</h1>
+        <section  class="presentation-conf">
+            <div class="contenu">
+                <?php
+                    $confrerie=array();
+                    for($i=0;$i<$confreries;$i++)
+                    {
+                        $confrerie[$i]=new Confrerie($tab[$i][0], $tab[$i][1]);
+                        $confrerie[$i]->affichage();
+                    }
+                ?>
+            </div>
+        </section>
+    </body>
+</html>
+
