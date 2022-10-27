a916e3f7b306a56da6064d95ea2e0d260649883c
Lytil <113536692+Lytil@users.noreply.github.com>
2022-10-27 10:55:11 +0200

    V1 Index
    V1

--- Versions/V1/footer.php
+++ Versions/V1/footer.php
@@ -1,20 +1,27 @@
 <html>
     <footer>
         <div class = "blocf">
-            <h5><a href="#">Sponsor</a></h5>
-            <h5><a href="index.php">Accueil</a></h5>
-        </div>
-        <div class = "blocf">
-            <h5><a href="#">Formulaire</a></h5>
-            <h5><a href="confrerie.php">Confreries</a></h5>
-        </div>
-        <div class = "blocf">
-            <h5><a href="#">Epreuves</a></h5>
-            <h5><a href="#">Programme</a></h5>
-        </div>
-        <div class = "blocf">
-            <h5><a href="#">Connexion</a></h5>
-            <h5></h5>
+            <h5><a href="#">Sponsors contactés</a></h5>
+            <div class="imgsponsor">
+                <img src="image/sponsors/pages.png">
+                <img src="image/sponsors/agglo.png">
+                <img src="image/sponsors/ville-le-puy.png">
+                <img src="image/sponsors/region.png">
+                <img src="image/sponsors/haute-loire-departement.png">
+                <img src="image/sponsors/Redbull.png">
+            </div>
+            <div class="blocnav">
+                <h5><a href="index.php">Accueil</a></h5>
+                <h5><a href="#">Formulaire</a></h5>
+                <h5><a href="confrerie.php">Confréries</a></h5>
+
+                <h5><a href="#">Epreuves</a></h5>
+                <h5><a href="#">Programme</a></h5>
+            </div>
+            <div class = "blocf btnadmin">
+                <h5><a href="#">Connexion</a></h5>
+                <h5></h5>
+            </div>
         </div>
     </footer>
 </html>
